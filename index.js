9d99cdddb411fcdd9590c8aa9a4efdcfd37b9035
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2021-01-12 16:17:11 -0600

    client folder changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("let's build!!!");
+const API_URL = 'http://localhost:3000/';
+
+fetch(API_URL)
+    .then(res => res.json())
+    .then(result => {
+        console.log(result);
+    });
